c4d5ae9a91ddac1d5d378b373018f00f8e4cf420
ramaraju.polanki <ramaraju.polanki@servicenow.com>
2021-08-27 10:01:51 +0530

    Added syso

--- MSNFullAutomation/src/main/java/com/msn/automation/MSNFullAutomation/App.java
+++ MSNFullAutomation/src/main/java/com/msn/automation/MSNFullAutomation/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello DevOps World!" );
         System.out.println( "Hello Servicenow!" );
+        System.out.println( "Added syso" );
     }
 }
